7747d55d6b995782c35e926072945936ec5d9629
Brandon Warhurst <roboknight@gmail.com>
2014-09-19 10:43:54 -0400

    Inital modifications to add LPC43xx/LPC4330-xplorer support

---
+++ os/hal/ports/NXP/LPC43xx/platform.mk
@@ -0,0 +1,7 @@
+# List of all platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
+              ${CHIBIOS}/os/hal/ports/NXP/LPC43xx/hal_lld.c 
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
+              ${CHIBIOS}/os/hal/ports/NXP/LPC43xx 
